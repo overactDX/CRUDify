68257a4ccbfe8b961451dbcf550de59cae27e5eb
Martins <overact150@gmail.com>
2024-02-15 19:00:20 +0700

    update

--- controller/Hello.js
+++ controller/Hello.js
@@ -1,9 +1,3 @@
 exports.hello = async (req, res) => {
-    try {
-      res.send('hello data');
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Server Error");
-    }
-  };
-  
+  res.send("hello data");
+};
